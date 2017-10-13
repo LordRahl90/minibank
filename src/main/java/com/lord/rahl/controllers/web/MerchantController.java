8f2881d96bad81e30c9bd605fde494a2ac2f8d23
Alugbin Abiodun <alugbin.abiodun@gmail.com>
2017-10-13 16:33:40 +0100

    Customer Login form

--- src/main/java/com/lord/rahl/controllers/web/MerchantController.java
+++ src/main/java/com/lord/rahl/controllers/web/MerchantController.java
@@ -1,8 +1,8 @@
 package com.lord.rahl.controllers.web;
 
 import com.lord.rahl.bean.CurrentMerchant;
+import com.lord.rahl.domain.Customer;
 import com.lord.rahl.domain.Merchant;
-import org.omg.CORBA.Current;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -18,17 +18,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MerchantController {
 
     @GetMapping("login")
-    public String login(Model model){
-        model.addAttribute("merchant",new Merchant());
+    public String login(Model model) {
+        model.addAttribute("merchant", new Merchant());
         return "merchants/login";
     }
 
     @GetMapping("/dashboard")
-    public String dashboard(Model model){
-        Authentication auth= SecurityContextHolder.getContext().getAuthentication();
-        CurrentMerchant currentMerchant=(CurrentMerchant) auth.getPrincipal();
-        System.out.println(auth.getDetails());
-        System.out.println(currentMerchant.getUsername());
+    public String dashboard(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        CurrentMerchant currentMerchant = (CurrentMerchant) auth.getPrincipal();
+        model.addAttribute("merchant", currentMerchant);
         return "merchants/dashboard";
+    }
+
+    @GetMapping("/new-customer")
+    public String newCustomer(Model model) {
+        model.addAttribute("customer",new Customer());
+        return "merchants/newCustomer";
     }
 }
--- src/main/java/com/lord/rahl/security/SecurityConfig.java
+++ src/main/java/com/lord/rahl/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/","/admin/**").permitAll()
-                .antMatchers("/merchants/**").hasAuthority("MERCHANT")
+                .antMatchers("/merchant/**").hasAuthority("MERCHANT")
                 .anyRequest().fullyAuthenticated()
                 .and()
                 .formLogin()
